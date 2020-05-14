bc093325d66c256ce6395d2323e9bc22b912da82
Lennart Heimbs <l.heimbs@gmail.com>
2020-05-14 18:14:03 +0200

    Add code sending to wake up pc

--- rf_handler.py
+++ rf_handler.py
@@ -8,11 +8,13 @@ import subprocess
 from rpi_rf import RFDevice
 import time
 
-GPIO_PIN = 20
+GPIO_PIN = 22
 CODES = {
     1: {"name": "pc",     "on": 1131857, "off": 1131860},
     2: {"name": "tablet", "on": 1134929, "off": 1134932},
-    3: {"name": "other",  "on": 1135697, "off": 1135700},
+    3: {"name": "rpi",  "on": 1135697, "off": 1135700},
+    4: {"name": "sensor",  "on": 1135889, "off": 1135892},
+    5: {"name": "other",  "on": 1135697, "off": 1135700},
 }
 SEND_PATH = "/home/pi/utils/433Utils/RPi_utils/send"
 
@@ -61,7 +63,21 @@ def send_code(socketnr, code):
         rf_device.enable_tx()
         rf_device.tx_repeat = 20
 
-        rf_device.tx_code(CODES[socketnr][code])
+        rf_device.tx_code(CODES[socketnr][code], tx_pulselength=500)
+    except:
+        logger.exception("Error while sending code to socket using rpi_rf")
+    finally:
+        rf_device.cleanup()
+
+
+def send_decimal(code):
+    logger.debug(f"'send_decimal' called. Sending code {code} using rpi_rf")
+    try:
+        rf_device = RFDevice(GPIO_PIN)
+        rf_device.enable_tx()
+        rf_device.tx_repeat = 20
+
+        rf_device.tx_code(code, tx_pulselength=500)
     except:
         logger.exception("Error while sending code to socket using rpi_rf")
     finally:
@@ -75,25 +91,41 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Optional Socket Control")
     parser.add_argument('-s', '--socket', default=0, type=int, choices=CODES, help="Socket number thats being controlled.")
     parser.add_argument('-c', '--cmd', default='on', choices=['on', 'off'], help="Turn socket on or off.")
+    parser.add_argument('-m', '--mode', default='rf', choices=['rf', 'sp'], help="Use rpi_rf or subcommand")
+    parser.add_argument('-d')
 
     args = parser.parse_args()
-    logger.debug("Arguments: socket=%s, command=%s", args.socket, args.cmd)
+    logger.debug("Arguments: socket=%s, command=%s method=%s", args.socket, args.cmd, args.mode)
 
+    if args.d:
+        logger.info("Sending Decimal code {}".format(args.d))
+        try:
+            rf_device = RFDevice(GPIO_PIN)
+            rf_device.enable_tx()
+            rf_device.tx_code(int(args.d))
+        except:
+            logger.exception("Error while sending code to socket using rpi_rf")
+        finally:
+            rf_device.cleanup()
 
-
-    if args.socket:
+    elif args.socket:
         if args.cmd == 'on':
-            turn_socket_on(args.socket, "rpi_rf")
+            if args.mode == 'rf':
+                turn_socket_on(args.socket, "rpi_rf")
+            else:
+                turn_socket_on(args.socket, "subprocess")
         else:
-            turn_socket_off(args.socket, "rpi_rf")
-
+            if args.mode == 'rf':
+                turn_socket_off(args.socket, "rpi_rf")
+            else:
+                turn_socket_off(args.socket, "subprocess")
     sys.exit()
     print(123)
 
     #cmd = sys.argv[1] if len(sys.argv) > 1 else ""
     #if cmd == "
-    
-    
+
+
     #turn_socket_on(3, "subprocess")
     #time.sleep(2)
     #turn_socket_off(3, "subprocess")
